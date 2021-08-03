858b1c6850fa33c0e1f2c05974768b1e0817722b
Adam CAVILLON <dandan2611@gameinbox.net>
2021-08-03 18:17:45 +0200

    Optimized ServerGui

--- src/main/java/fr/wydavix/WydavixTools/gui/ServerGui.java
+++ src/main/java/fr/wydavix/WydavixTools/gui/ServerGui.java
@@ -1,22 +1,29 @@
 package fr.wydavix.wydavixtools.gui;
 
+import fr.wydavix.wydavixtools.WydavixUtils;
 import org.bukkit.Bukkit;
 import org.bukkit.Material;
 import org.bukkit.inventory.Inventory;
 import org.bukkit.inventory.ItemStack;
 
-import fr.wydavix.WydavixTools.PluginsManager.Main;
-
 public class ServerGui {
 
-	private ItemStack back = Main.instance.Utils.CreateItem(Material.ARROW, 0, "�f�l� �4Retour");
+	private static final ItemStack BACK_ITEM = WydavixUtils.createItem(Material.ARROW, 0, "§f§l» §4Retour");
+
+	private final GuiManager manager;
+
+	public ServerGui(GuiManager manager) {
+		this.manager = manager;
+	}
 
 	public Inventory getMainServerGui() {
-		Inventory MainServerGui = Bukkit.createInventory(null, 45, "�c�k!!�4Server Menu�c�k!!");
+		Inventory inventory = Bukkit.createInventory(null, 45, "§c§k!!§4Server Menu§c§k!!");
 
-		Main.instance.GuiManager.setGlassInventory(MainServerGui);
-		MainServerGui.setItem(36, back);
+		manager.fillInventoryWithGlass(inventory);
 
-		return MainServerGui;
+		inventory.setItem(36, BACK_ITEM);
+
+		return inventory;
 	}
+
 }
