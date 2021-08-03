24050b4788873d40c30515d617a7a8b9d94bcb50
Adam CAVILLON <dandan2611@gameinbox.net>
2021-08-03 17:33:42 +0200

    Documented Utils class

--- src/main/java/fr/wydavix/wydavixtools/WydavixUtils.java
+++ src/main/java/fr/wydavix/wydavixtools/WydavixUtils.java
@@ -10,6 +10,14 @@ import org.bukkit.inventory.meta.SkullMeta;
 
 public class WydavixUtils {
 
+	/**
+	 * Create a skull item
+	 * @param materialShort Type of head
+	 * @param name DisplayName of the head
+	 * @param player If the head is a player head, set the skin to the player's one
+	 * @param lore Item lore
+	 * @return Fresh {@link ItemStack} with given informations
+	 */
 	public ItemStack createSkullItem(byte materialShort, String name, Player player, String... lore) {
 		ItemStack item = new ItemStack(Material.SKULL_ITEM, 1, (byte) materialShort);
 		SkullMeta meta = (SkullMeta) item.getItemMeta();
@@ -23,6 +31,13 @@ public class WydavixUtils {
 		return item;
 	}
 
+	/**
+	 * Create an item
+	 * @param material Material of the item
+	 * @param materialShort Type of the item
+	 * @param name Name of the item
+	 * @return Fresh {@link ItemStack} with given informations
+	 */
 	public ItemStack createItem(Material material, int materialShort, String name) {
 		ItemStack item = new ItemStack(material, 1, (byte) materialShort);
 		ItemMeta itemMeta = item.getItemMeta();
@@ -34,6 +49,14 @@ public class WydavixUtils {
 		return item;
 	}
 
+	/**
+	 * Create an item
+	 * @param material Material of the item
+	 * @param materialShort Type of the item
+	 * @param name Name of the item
+	 * @param lore Lore of the item
+	 * @return Fresh {@link ItemStack} with given informations
+	 */
 	public ItemStack createItem(Material material, int materialShort, String name, String... lore) {
 		ItemStack item = new ItemStack(material, 1, (byte) materialShort);
 		ItemMeta itemMeta = item.getItemMeta();
