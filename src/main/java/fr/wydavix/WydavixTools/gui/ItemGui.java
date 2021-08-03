75d3928cd4cc3b983a77a38a3bb7b93567a43243
Adam CAVILLON <dandan2611@gameinbox.net>
2021-08-03 18:51:33 +0200

    Revamped ItemGui

--- src/main/java/fr/wydavix/WydavixTools/gui/ItemGui.java
+++ src/main/java/fr/wydavix/WydavixTools/gui/ItemGui.java
@@ -1,71 +1,103 @@
 package fr.wydavix.wydavixtools.gui;
 
+import fr.wydavix.wydavixtools.WydavixUtils;
 import org.bukkit.Bukkit;
 import org.bukkit.Material;
 import org.bukkit.inventory.Inventory;
 import org.bukkit.inventory.ItemStack;
 
-import fr.wydavix.WydavixTools.PluginsManager.Main;
-
 public class ItemGui {
-	private ItemStack back = Main.instance.Utils.CreateItem(Material.ARROW, 0, "�f�l� �4Retour");
-	private ItemStack page1 = Main.instance.Utils.CreateItem(Material.STAINED_GLASS_PANE, 14, "�f�l� �cPage 1");
 
-	public Inventory CreateMainItemGui() {
+	private static final ItemStack BACK_ITEM = ServerGui.BACK_ITEM;
+	private static final ItemStack PAGE_ONE_ITEM = WydavixUtils.createItem(
+			Material.STAINED_GLASS_PANE,
+			14,
+			"§f§l» §cPage 1"
+	);
 
-		Inventory MainItemGui = Bukkit.createInventory(null, 45, "�d�k!!�5Items Managers�d�k!!");
+	private static final ItemStack ARMOR_ITEM = WydavixUtils.createItem(
+			Material.DIAMOND_CHESTPLATE,
+			0,
+			"§4§l» §cArmor"
+	);
+	private static final ItemStack TOOL_ITEM = WydavixUtils.createItem(
+			Material.DIAMOND_AXE,
+			0,
+			"§3§l» §9Tool"
+	);
+	private static final ItemStack OTHER_ITEM = WydavixUtils.createItem(
+			Material.COBBLESTONE,
+			0,
+			"§6§l» §eOther"
+	);
 
-		ItemStack armor = Main.instance.Utils.CreateItem(Material.DIAMOND_CHESTPLATE, 0, "�4�l� �cArmor");
-		ItemStack tool = Main.instance.Utils.CreateItem(Material.DIAMOND_AXE, 0, "�3�l� �9Tool");
-		ItemStack other = Main.instance.Utils.CreateItem(Material.COBBLESTONE, 0, "�6�l� �eOther");
+	private final GuiManager manager;
 
-		Main.instance.GuiManager.setGlassInventory(MainItemGui);
-		MainItemGui.setItem(20, armor);
-		MainItemGui.setItem(22, other);
-		MainItemGui.setItem(24, tool);
-		MainItemGui.setItem(36, back);
-
-		return MainItemGui;
+	public ItemGui(GuiManager manager) {
+		this.manager = manager;
 	}
 
-	public Inventory ArmorPage1ItemGui() {
+	public Inventory generateMainItemGui() {
+		Inventory inventory = Bukkit.createInventory(null, 45, "§d§k!!§5Items Managers§d§k!!");
 
-		Inventory ArmorPageItemGui = Bukkit.createInventory(null, 45, "�4�l� �cArmor Page 1");
+		manager.fillInventoryWithGlass(inventory);
 
-		Main.instance.GuiManager.setGlassInventory(ArmorPageItemGui);
+		inventory.setItem(20, ARMOR_ITEM);
+		inventory.setItem(22, OTHER_ITEM);
+		inventory.setItem(24, TOOL_ITEM);
+		inventory.setItem(36, BACK_ITEM);
 
-		ArmorPageItemGui.setItem(36, back);
-		ArmorPageItemGui.setItem(38, page1);
-		ArmorPageItemGui.setItem(44, Main.instance.Utils.CreateItem(Material.REDSTONE, 0, "�f�l� �eOther"));
-
-		return ArmorPageItemGui;
+		return inventory;
 	}
 
-	public Inventory ToolPage1ItemGui() {
+	private static final ItemStack ARMOR_OTHER_ITEM = WydavixUtils.createItem(
+			Material.REDSTONE,
+			0,
+			"§f§l» §eOther"
+	);
 
-		Inventory ToolPageItemGui = Bukkit.createInventory(null, 45, "�3�l� �9Tool Page 1");
+	public Inventory generateArmorPageGui() {
+		Inventory inventory = Bukkit.createInventory(null, 45, "§4§l» §cArmor Page 1");
 
-		Main.instance.GuiManager.setGlassInventory(ToolPageItemGui);
+		manager.fillInventoryWithGlass(inventory);
 
-		ToolPageItemGui.setItem(10, Main.instance.ItemManager.HammerItem.getHammerFake());
-		ToolPageItemGui.setItem(11, Main.instance.ItemManager.MjolnirItem.getMjolnirFake());
-		ToolPageItemGui.setItem(36, back);
-		ToolPageItemGui.setItem(38, page1);
-		ToolPageItemGui.setItem(44, Main.instance.Utils.CreateItem(Material.REDSTONE, 0, "�f�l� �cArmor"));
+		inventory.setItem(36, BACK_ITEM);
+		inventory.setItem(38, PAGE_ONE_ITEM);
+		inventory.setItem(44, ARMOR_OTHER_ITEM);
 
-		return ToolPageItemGui;
+		return inventory;
 	}
 
-	public Inventory OtherPage1ItemGui() {
+	public Inventory generateToolPageGui() {
+		Inventory inventory = Bukkit.createInventory(null, 45, "§3§l» §9Tool Page 1");
 
-		Inventory OtherPageItemGui = Bukkit.createInventory(null, 45, "�6�l� �eOther Page 1");
+		manager.fillInventoryWithGlass(inventory);
 
-		Main.instance.GuiManager.setGlassInventory(OtherPageItemGui);
+		inventory.setItem(10, null); // TODO: Fake hammer item
+		inventory.setItem(11, null); // TODO: Fake Mjolnir item
+		inventory.setItem(36, BACK_ITEM);
+		inventory.setItem(38, PAGE_ONE_ITEM);
+		inventory.setItem(44, ARMOR_OTHER_ITEM);
 
-		OtherPageItemGui.setItem(36, back);
-		OtherPageItemGui.setItem(38, page1);
-		OtherPageItemGui.setItem(44, Main.instance.Utils.CreateItem(Material.REDSTONE, 0, "�f�l� �9Tool"));
-
-		return OtherPageItemGui;
+		return inventory;
 	}
+
+	private static final ItemStack OTHER_TOOL_ITEM = WydavixUtils.createItem(
+			Material.REDSTONE,
+			0,
+			"§f§l» §9Tool"
+	);
+
+	public Inventory generateOtherPageGui() {
+		Inventory inventory = Bukkit.createInventory(null, 45, "§6§l» §eOther Page 1");
+
+		manager.fillInventoryWithGlass(inventory);
+
+		inventory.setItem(36, BACK_ITEM);
+		inventory.setItem(38, PAGE_ONE_ITEM);
+		inventory.setItem(44, OTHER_TOOL_ITEM);
+
+		return inventory;
+	}
+
 }
